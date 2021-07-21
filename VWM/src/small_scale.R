ccf406febb4bce1f7ced84c9256aa11d17eb6986
Jia Li <jia@instance-1.us-central1-a.c.folkloric-rite-308414.internal>
2021-07-21 17:32:22 +0000

    vwm

--- VWM/src/small_scale.R
+++ VWM/src/small_scale.R
@@ -4,7 +4,7 @@ exp1_dt <- readRDS('./VWM/data/processed/OL_exp1.rds')
 i <- 1
 ind <- exp1_dt$ID==i
 
-prior_ind <- 1
+prior_ind <- 2
 prior_file <- paste0('prior_',prior_ind)
 pw <- paste0("./VWM/output/results/small_scale/",
              prior_file)
