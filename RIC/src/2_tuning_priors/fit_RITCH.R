033540e9f93dd2c43a0bec72c5d4d5f91f29f88e
Jia Li <jia@instance-1.us-central1-a.c.folkloric-rite-308414.internal>
2021-09-27 14:45:19 +0000

    RIC fit new code

--- RIC/src/2_tuning_priors/fit_RITCH.R
+++ RIC/src/2_tuning_priors/fit_RITCH.R
@@ -4,6 +4,72 @@ library(rstan)
 options(mc.cores = parallel::detectCores())
 Sys.setenv(STAN_NUM_THREADS = 4)
 
+# Indifference point -------------------
+indiff_set <- read_csv("./RIC/data/previous/Indiff.csv")%>%
+  add_column(Paradigm='Indiff',t1=0,p1=1)%>%
+  mutate(y=round(N/2))%>%
+  rename(x1=Indifferences,x2=Amounts,t2=Delay,p2=Probability)%>%
+  dplyr::select(Paradigm,Exp,x1,p1,t1,x2,p2,t2,N,y)%>%
+  mutate(xd = x1-x2,td = t2-t1, pd = p1-p2)%>%
+  mutate(xs = sign(xd),ts = sign(td),
+         ps = sign(pd),
+         xr = 2*xd/(x1+x2),
+         tr = ifelse(td==0,0,2*td/(t1+t2)),
+         pr = 2*pd/(p1+p2))
+table(indiff_set$Exp)
+
+parameters <- c('beta_xo','beta_xa','beta_xr',
+                'beta_po','beta_pa','beta_pr',
+                'beta_to','beta_ta','beta_tr')
+
+## RIC ================
+indiff_RI <- indiff_set%>%filter(t2>0,p2<1)
+unique(indiff_RI$Exp)
+i <- unique(indiff_RI$Exp)[3]
+i  
+
+indiff_temp <- indiff_set%>%filter(Exp==i)
+data<-list(
+    nTrial=nrow(indiff_temp),N = indiff_temp$N,
+    xs = indiff_temp$xs,ts = indiff_temp$ts,ps = indiff_temp$ps,
+    xd = indiff_temp$xd,td = indiff_temp$td,pd = indiff_temp$pd,
+    xr = indiff_temp$xr,tr = indiff_temp$tr,pr = indiff_temp$pr,
+    y = indiff_temp$y)
+samples <- stan(file='./RIC/src/2_tuning_priors/fit_RITCH_1.stan',
+                  data=data,
+                  pars=parameters,
+                  iter = 8000,
+                  warmup = 4000,
+                  chains = 4, 
+                  thin = 4,
+                  cores = 4,
+                  seed = 123,
+                  verbose = TRUE,
+                  refresh = 100,
+                  control = list(max_treedepth = 15))
+saveRDS(samples,paste0('./RIC/output/results/fit_prev/RITCH_indiff_',i,'.rds'))
+
+
+png(paste0('./RIC/output/fig/fit_prev/RITCH_indiff_pairs_',i,'.png'),
+    width = 6, height = 6, units = 'in', res = 300)
+par(mar=c(1,1,1,1))
+pairs(samples,pars = parameters)
+dev.off()
+
+png(paste0('./RIC/output/fig/fit_prev/RITCH_indiff_trace_',i,'.png'),
+    width = 6, height = 6, units = 'in', res = 300)
+par(mar=c(1,1,1,1))
+traceplot(samples, pars = parameters)
+dev.off()
+
+post_stasts <- summary(samples)
+write.csv(post_stasts$summary,
+          paste0('./RIC/output/results/fit_prev/RITCH_indiff_',i,'.csv'))
+
+## Delay ==============
+
+## Risky ==============
+
 # available data sets ------------
 ## money below 15000, delay below 6 years
 retr_set <- read_csv("./RIC/data/previous/Retrieval.csv")%>%
@@ -62,70 +128,6 @@ pairs(samples,pars = parameters)
 dev.off()
 
 png("./RIC/output/fig/fit_prev/RITCH_retr_trace.png",
-    width = 6, height = 6, units = 'in', res = 300)
-par(mar=c(1,1,1,1))
-traceplot(samples, pars = parameters)
-dev.off()
-
-# Indifference point -------------------
-indiff_set <- read_csv("./RIC/data/previous/Indiff.csv")%>%
-  add_column(Paradigm='Indiff',t1=0,p1=1)%>%
-  mutate(y=round(N/2))%>%
-  rename(x1=Indifferences,x2=Amounts,t2=Delay,p2=Probability)%>%
-  dplyr::select(Paradigm,Exp,x1,p1,t1,x2,p2,t2,N,y)%>%
-  mutate(xd = x1-x2,td = t2-t1, pd = p1-p2)%>%
-  mutate(xs = sign(xd),ts = sign(td),
-         ps = sign(pd),
-         xr = 2*xd/(x1+x2),
-         tr = ifelse(td==0,0,2*td/(t1+t2)),
-         pr = 2*pd/(p1+p2))
-
-indiff_set$Exp_ind <- rep(0,length(indiff_set$Exp))
-Set_list <- unique(indiff_set$Exp)
-for(i in 1:length(Set_list)){
-  ind <- indiff_set$Exp==Set_list[i]
-  indiff_set$Exp_ind[ind] <- i
-}
-parameters <- c('beta_xo','beta_xa','beta_xr',
-                'beta_po','beta_pa','beta_pr',
-                'beta_to','beta_ta','beta_tr',
-                'beta_xo_i','beta_xa_i','beta_xr_i',
-                'beta_po_i','beta_pa_i','beta_pr_i',
-                'beta_to_i','beta_ta_i','beta_tr_i',
-                'SD_i')
-
-data<-list(
-	nTrial=nrow(indiff_set),nExp=length(Set_list),
-	N = indiff_set$N, Exp = indiff_set$Exp_ind,
-	xs = indiff_set$xs,ts = indiff_set$ts,ps = indiff_set$ps,
-	xd = indiff_set$xd,td = indiff_set$td,pd = indiff_set$pd,
-	xr = indiff_set$xr,tr = indiff_set$tr,pr = indiff_set$pr,
-	y = indiff_set$y)
-samples <- stan(file='./RIC/src/2_tuning_priors/fit_RITCH_2.stan',
-                  data=data,
-                  pars=parameters,
-                  iter=8000,
-                  warmup = 4000,
-                  chains=4, 
-                  thin=4,
-                  cores=4,
-                  seed = 123,
-                verbose = TRUE,
-                refresh = 50,
-                control = list(max_treedepth = 15))
-saveRDS(samples,
-        './RIC/output/results/fit_prev/RITCH_indiff.rds')
-post_stasts <- summary(samples)
-write.csv(post_stasts$summary,
-          './RIC/output/results/fit_prev/RITCH_indiff_stats.csv')
-## posterior ==================
-png("./RIC/output/fig/fit_prev/RITCH_indiff_pairs.png",
-    width = 6, height = 6, units = 'in', res = 300)
-par(mar=c(1,1,1,1))
-pairs(samples,pars = parameters)
-dev.off()
-
-png("./RIC/output/fig/fit_prev/RITCH_indiff_trace.png",
     width = 6, height = 6, units = 'in', res = 300)
 par(mar=c(1,1,1,1))
 traceplot(samples, pars = parameters)
--- RIC/src/2_tuning_priors/fit_RITCH_1.stan
+++ RIC/src/2_tuning_priors/fit_RITCH_1.stan
@@ -7,9 +7,7 @@ functions{
 }
 data{
   int<lower=1> nTrial;
-  int<lower=1> nExp;
   int<lower=1> N[nTrial];
-  int<lower=1,upper=nExp> Exp[nTrial];
   vector<lower=-1,upper=1>[nTrial] xs;
   vector[nTrial] xd;
   vector[nTrial] xr;
@@ -31,34 +29,22 @@ parameters{
   real<lower=0> beta_tr;
   real<lower=0> beta_pa;
   real<lower=0> beta_pr;
-  
-  real<lower=0> SD_i;
-  vector<lower=0>[nExp] beta_xo_i;
-  vector<lower=0>[nExp] beta_to_i;
-  vector<lower=0>[nExp] beta_po_i;
-  vector<lower=0>[nExp] beta_xa_i;
-  vector<lower=0>[nExp] beta_ta_i;
-  vector<lower=0>[nExp] beta_xr_i;
-  vector<lower=0>[nExp] beta_tr_i;
-  vector<lower=0>[nExp] beta_pa_i;
-  vector<lower=0>[nExp] beta_pr_i;
 }
 transformed parameters{
-  vector<lower=0>[nExp] sd_i = rep_vector(SD_i,nExp);
   vector[nTrial] X;
   vector[nTrial] TT;
   vector[nTrial] R;
   real theta_logit[nTrial];   
 	for(j in 1:nTrial){
-	  X[j] = beta_xo_i[Exp[j]]*xs[j]+beta_xa_i[Exp[j]]*xd[j]+beta_xr_i[Exp[j]]*xr[j];
-	  TT[j] = beta_to_i[Exp[j]]*ts[j]+beta_ta_i[Exp[j]]*td[j]+beta_tr_i[Exp[j]]*tr[j];
-	  R[j] = beta_po_i[Exp[j]]*ps[j]+beta_pa_i[Exp[j]]*pd[j]+beta_pr_i[Exp[j]]*pr[j];
+	  X[j] = beta_xo*xs[j]+beta_xa*xd[j]+beta_xr*xr[j];
+	  TT[j] = beta_to*ts[j]+beta_ta*td[j]+beta_tr*tr[j];
+	  R[j] = beta_po*ps[j]+beta_pa*pd[j]+beta_pr*pr[j];
   }
   theta_logit = to_array_1d(fmax(fmin(X+TT+R,10),-10));
 }
 model{
   int grainsize=1;
-  //priors of group parameters
+  //priors
   beta_xo ~ normal(0,10);
   beta_po ~ normal(0,10);
   beta_to ~ normal(0,10);
@@ -68,18 +54,6 @@ model{
   beta_pr ~ normal(0,10);
   beta_ta ~ normal(0,10);
   beta_tr ~ normal(0,10);
-  
-  //priors of individual parameters
-  SD_i ~ cauchy(0,10);
-  beta_xo_i ~ normal(beta_xo,sd_i);
-  beta_po_i ~ normal(beta_po,sd_i);
-  beta_to_i ~ normal(beta_to,sd_i);
-  beta_xa_i ~ normal(beta_xa,sd_i);
-  beta_xr_i ~ normal(beta_xr,sd_i);
-  beta_pa_i ~ normal(beta_pa,sd_i);
-  beta_pr_i ~ normal(beta_pr,sd_i);
-  beta_ta_i ~ normal(beta_ta,sd_i);
-  beta_tr_i ~ normal(beta_tr,sd_i);
   
   //likelihood
   target += reduce_sum(partial_sum,y,grainsize,theta_logit,N);
