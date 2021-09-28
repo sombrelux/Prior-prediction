1d83077f3d55f362425d6e3ecd338095c8ec86c2
sombrelux <2423354032@qq.com>
2021-09-28 10:40:10 +0200

    RIC fit RITCH delay

--- RIC/src/2_tuning_priors/fit_RITCH.R
+++ RIC/src/2_tuning_priors/fit_RITCH.R
@@ -2,7 +2,128 @@ rm(list=ls())
 library(tidyverse)
 library(rstan)
 options(mc.cores = parallel::detectCores())
-Sys.setenv(STAN_NUM_THREADS = 4)
+#Sys.setenv(STAN_NUM_THREADS = 4)
+
+# available data sets ------------
+## money below 15000, delay below 6 years
+retr_set <- read_csv("./RIC/data/previous/Retrieval.csv")%>%
+  dplyr::select(Exp,x1,p1,t1,x2,p2,t2,N,y)%>%
+  mutate(xd = x1-x2,td = t2-t1, pd = p1-p2)%>%
+  mutate(xs = sign(xd),ts = sign(td),
+         ps = sign(pd),
+         xr = 2*xd/(x1+x2),
+         tr = ifelse(td==0,0,2*td/(t1+t2)),
+         pr = 2*pd/(p1+p2))
+
+table(retr_set$Exp)
+## delay =============
+#for(i in retr_delay$Exp){
+#i <- unique(retr_delay$Exp)[1]
+#i  
+#retr_temp <- retr_delay%>%filter(Exp==i)
+
+retr_delay <- retr_set%>%filter(t2>0,p2==1) #delay
+table(retr_delay$Exp)
+
+Set_list <- unique(retr_delay$Exp)
+retr_delay$Exp_ind <- rep(0,length(retr_delay$Exp))
+for(i in 1:length(Set_list)){
+  retr_delay$Exp_ind[retr_delay$Exp==Set_list[i]] <- i
+}
+
+parameters <- c('beta_o','beta_xa','beta_xr',
+                'beta_ta','beta_tr','sd_i',
+				'beta_o_i','beta_xa_i','beta_xr_i',
+                'beta_ta_i','beta_tr_i')
+
+data<-list(
+  nExp = length(Set_list),Exp = retr_delay$Exp_ind,
+  nTrial=nrow(retr_delay),N = retr_delay$N,
+  xd = retr_delay$xd,td = retr_delay$td,
+  xr = retr_delay$xr,tr = retr_delay$tr,
+  y = retr_delay$y)
+
+samples <- stan(file = './RIC/src/2_tuning_priors/fit_RITCH_delay.stan',
+                data = data,
+                pars = parameters,
+                iter = 2000,
+                warmup = 1000,
+                chains = 4, 
+                thin = 4,
+                cores = 4,
+                seed = 123,
+                verbose = TRUE,
+                refresh = 50,
+                control = list(max_treedepth = 15))
+saveRDS(samples, 
+        paste0('./RIC/output/results/fit_prev/RITCH_',i,'.rds'))
+post_stasts <- summary(samples)
+write.csv(post_stasts$summary,
+          paste0('./RIC/output/results/fit_prev/RITCH_',i,'.csv'))
+
+png(paste0('./RIC/output/fig/fit_prev/Pairs_RITCH_',i,'.png'),
+    width = 6, height = 6, units = 'in', res = 300)
+par(mar=c(1,1,1,1))
+pairs(samples,pars = parameters)
+dev.off()
+
+png(paste0('./RIC/output/fig/fit_prev/Trace_RITCH_',i,'.png'),
+    width = 6, height = 6, units = 'in', res = 300)
+par(mar=c(1,1,1,1))
+traceplot(samples, pars = parameters)
+dev.off()
+
+## risky ==================
+retr_risky <- retr_set%>%filter(t2==0,p2<1) 
+table(retr_risky$Exp)
+
+parameters <- c('beta_o','beta_xa','beta_xr',
+                'beta_pa','beta_pr')
+
+#for(i in retr_risky$Exp){
+i <- unique(retr_risky$Exp)[1]
+i  
+retr_temp <- retr_risky%>%filter(Exp==i)
+data<-list(
+    nTrial=nrow(retr_temp),
+    N = retr_temp$N,
+    xd = retr_temp$xd,pd = retr_temp$pd,
+    xr = retr_temp$xr,pr = retr_temp$pr,
+    y = retr_temp$y)
+
+samples <- stan(file = './RIC/src/2_tuning_priors/fit_RITCH_risky.stan',
+                data = data,
+                pars = parameters,
+                iter = 2000,
+                warmup = 1000,
+                chains = 4, 
+                thin = 4,
+                cores = 4,
+                seed = 123,
+                verbose = TRUE,
+                refresh = 50,
+                control = list(max_treedepth = 15))
+saveRDS(samples, 
+        paste0('./RIC/output/results/fit_prev/RITCH_',i,'.rds'))
+post_stasts <- summary(samples)
+write.csv(post_stasts$summary,
+          paste0('./RIC/output/results/fit_prev/RITCH_',i,'.csv'))
+
+png(paste0('./RIC/output/fig/fit_prev/Pairs_RITCH_',i,'.png'),
+    width = 6, height = 6, units = 'in', res = 300)
+par(mar=c(1,1,1,1))
+pairs(samples,pars = parameters)
+dev.off()
+
+png(paste0('./RIC/output/fig/fit_prev/Trace_RITCH_',i,'.png'),
+    width = 6, height = 6, units = 'in', res = 300)
+par(mar=c(1,1,1,1))
+traceplot(samples, pars = parameters)
+dev.off()
+
+## RIC =============
+
+
 
 # Indifference point -------------------
 indiff_set <- read_csv("./RIC/data/previous/Indiff.csv")%>%
@@ -69,66 +190,3 @@ write.csv(post_stasts$summary,
 ## Delay ==============
 
 ## Risky ==============
-
-# available data sets ------------
-## money below 15000, delay below 6 years
-retr_set <- read_csv("./RIC/data/previous/Retrieval.csv")%>%
-  dplyr::select(Exp,x1,p1,t1,x2,p2,t2,N,y)%>%
-  mutate(xd = x1-x2,td = t2-t1, pd = p1-p2)%>%
-  mutate(xs = sign(xd),ts = sign(td),
-         ps = sign(pd),
-         xr = 2*xd/(x1+x2),
-         tr = ifelse(td==0,0,2*td/(t1+t2)),
-         pr = 2*pd/(p1+p2))
-
-retr_set$Exp_ind <- rep(0,length(retr_set$Exp))
-Set_list <- unique(retr_set$Exp)
-for(i in 1:length(Set_list)){
-  ind <- retr_set$Exp==Set_list[i]
-  retr_set$Exp_ind[ind] <- i
-}
-parameters <- c('beta_xo','beta_xa','beta_xr',
-                'beta_po','beta_pa','beta_pr',
-                'beta_to','beta_ta','beta_tr',
-                'beta_xo_i','beta_xa_i','beta_xr_i',
-                'beta_po_i','beta_pa_i','beta_pr_i',
-                'beta_to_i','beta_ta_i','beta_tr_i',
-                'SD_i')
-
-data<-list(
-  nTrial=nrow(retr_set),nExp=length(Set_list),
-  N = retr_set$N, Exp = retr_set$Exp_ind,
-  xs = retr_set$xs,ts = retr_set$ts,ps = retr_set$ps,
-  xd = retr_set$xd,td = retr_set$td,pd = retr_set$pd,
-  xr = retr_set$xr,tr = retr_set$tr,pr = retr_set$pr,
-  y = retr_set$y)
-
-samples <- stan(file = './RIC/src/2_tuning_priors/fit_RITCH_1.stan',
-                data = data,
-                pars = parameters,
-                iter = 8000,
-                warmup = 4000,
-                chains = 4, 
-                thin = 4,
-                cores = 4,
-                seed = 123,
-                verbose = TRUE,
-                refresh = 50,
-                control = list(max_treedepth = 15))
-saveRDS(samples, './RIC/output/results/fit_prev/RITCH_retr.rds')
-post_stasts <- summary(samples)
-write.csv(post_stasts$summary,
-          './RIC/output/results/fit_prev/RITCH_retr_stats.csv')
-
-## posterior ==================
-png("./RIC/output/fig/fit_prev/RITCH_retr_pairs.png",
-    width = 6, height = 6, units = 'in', res = 300)
-par(mar=c(1,1,1,1))
-pairs(samples,pars = parameters)
-dev.off()
-
-png("./RIC/output/fig/fit_prev/RITCH_retr_trace.png",
-    width = 6, height = 6, units = 'in', res = 300)
-par(mar=c(1,1,1,1))
-traceplot(samples, pars = parameters)
-dev.off()
