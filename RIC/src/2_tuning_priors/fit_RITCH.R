634100b39d23d7568d497884e6e46a3dd82bf540
Jia Li <jia@instance-1.us-central1-a.c.folkloric-rite-308414.internal>
2021-10-01 08:35:14 +0000

    RIC fit prev

--- RIC/src/2_tuning_priors/fit_RITCH.R
+++ RIC/src/2_tuning_priors/fit_RITCH.R
@@ -35,7 +35,7 @@ for(i in 1:length(Set_list)){
 
 parameters <- c('beta_o','beta_xa','beta_xr',
                 'beta_ta','beta_tr','sd_i',
-				'beta_o_i','beta_xa_i','beta_xr_i',
+                'beta_o_i','beta_xa_i','beta_xr_i',
                 'beta_ta_i','beta_tr_i')
 
 data<-list(
@@ -48,14 +48,14 @@ data<-list(
 samples <- stan(file = './RIC/src/2_tuning_priors/fit_RITCH_delay.stan',
                 data = data,
                 pars = parameters,
-                iter = 2000,
-                warmup = 1000,
+                iter = 4000,
+                warmup = 2000,
                 chains = 4, 
                 thin = 4,
                 cores = 4,
                 seed = 123,
                 verbose = TRUE,
-                refresh = 50,
+                refresh = 100,
                 control = list(max_treedepth = 15))
 saveRDS(samples, 
         './RIC/output/results/fit_prev/RITCH_delay.rds')
@@ -118,7 +118,7 @@ samples <- stan(file = './RIC/src/2_tuning_priors/fit_RITCH_risky.stan',
                 cores = 4,
                 seed = 123,
                 verbose = TRUE,
-                refresh = 50,
+                refresh = 100,
                 control = list(max_treedepth = 15))
 saveRDS(samples, 
         './RIC/output/results/fit_prev/RITCH_risky.rds')
@@ -170,7 +170,8 @@ parameters <- c('beta_xo','beta_xa','beta_xr',
                 'beta_to','beta_ta','beta_tr')
 Set_list <- unique(ric_set$Exp)
 Set_list
-for(i in Set_list){
+#for(i in Set_list){
+i <- Set_list[1]
   ric_temp <- ric_set%>%filter(Exp==i)
   data<-list(
     nTrial=nrow(ric_temp),N = ric_temp$N,
@@ -193,8 +194,8 @@ for(i in Set_list){
   saveRDS(samples,
           paste0('./RIC/output/results/fit_prev/RITCH_',
                  i,'.rds'))
-}
-
+#}
+  pairs(samples)
 
 ## hier ================
 Set_list <- unique(ric_set$Exp)
