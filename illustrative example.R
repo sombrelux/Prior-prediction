59516113d4c3d9635a5becdbc6d26cedd4291172
sombrelux <2423354032@qq.com>
2022-01-09 03:23:39 +0000

    illust

--- illustrative example.R
+++ illustrative example.R
@@ -3,12 +3,12 @@ n <- 100
 N <- 100000
 
 resp_real <- resp_vague <- matrix(nrow=n,ncol=N)
+prob_real <- 1/(1+exp(-1))
+resp_real <- rbinom(n*N,1,prob_real)
+
 for(i in 1:n){
-  a_real <- runif(N,0,1)
   a_vague <- runif(N,0,100)
-  prob_real <- 1/(1+exp(-a_real))
   prob_vague <- 1/(1+exp(-a_vague))
-  resp_real[i,] <- sapply(prob_real, function(u) rbinom(1,1,u))
   resp_vague[i,] <- sapply(prob_vague, function(u) rbinom(1,1,u))
 }
 
