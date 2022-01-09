85ad7578986d70f55ff8454e2de9c116294b92dd
sombrelux <2423354032@qq.com>
2022-01-09 09:58:16 +0100

    local

--- VWM/src/4_core_pred_unfit/specify_prior.R
+++ VWM/src/4_core_pred_unfit/specify_prior.R
@@ -1,29 +1,6 @@
 rm(list=ls())
 library(tidyverse)
 
-# sd2k ------------
-sd2k <- function(s){
-  R = exp(-s^2/2)
-  k = 1/(R^3-4*R^2+3*R)
-  if((0.53<=R) & (R<0.85)) k = -0.4+1.39*R+0.43/(1-R)
-  if(R < 0.53) k = 2*R+R^3+(5*R^5)/6
-  return(k)
-}
-sd2k(0.29+3*0.03) #7.46
-sd2k(0.29-3*0.03) #25.51
-
-sd2k(0.6-3*0.04) #4.89
-sd2k(0.6+3*0.04) #2.56
-
-sd2k(22.2/180*pi) #7.19
-sd2k(9.1/180*pi) #40.15
-
-# s ---------
-exp(-10)#~e-5
-exp(-20)#~e-9
-exp(-30)#~e-14
-
-# std -----------
 rtruncnorm <- function(n,mu,sig,lb,ub){
   ulb <- pnorm(lb,mu,sig)
   uub <- pnorm(ub,mu,sig)
@@ -32,13 +9,11 @@ rtruncnorm <- function(n,mu,sig,lb,ub){
   return(x)
 }
 n <- 10000000
-
+sig_scale <- c(0.05,0.1,0.5,1)
 post_param <- read_csv('./VWM/output/results/fit_prev/param_im.csv')
 mu_post <- signif(post_param$mean,2)
 mu_post
 
-sd_post <- signif(post_param$sd,1)
-sd_post
 
 ## a ---------------
 mu <- 0.16; lb <- 0; ub <- 1
@@ -83,3 +58,44 @@ sd(delta)
 # w ---------------
 w <- rbeta(n,2,1)
 hist(w)
+
+
+# sd2k ------------
+sd2k <- function(s){
+  R = exp(-s^2/2)
+  k = 1/(R^3-4*R^2+3*R)
+  if((0.53<=R) & (R<0.85)) k = -0.4+1.39*R+0.43/(1-R)
+  if(R < 0.53) k = 2*R+R^3+(5*R^5)/6
+  return(k)
+}
+sd2k(0.29) #12.41
+sd2k(0.29+3*0.03) #7.46
+sd2k(0.29-3*0.03) #25.51
+
+sd2k(0.6-3*0.04) #4.89
+sd2k(0.6+3*0.04) #2.56
+
+sd2k(22.2/180*pi) #7.19
+sd2k(9.1/180*pi) #40.15
+
+# s ---------
+exp(-10)#~e-5
+exp(-20)#~e-9
+exp(-30)#~e-14
+
+# std -----------
+rtruncnorm <- function(n,mu,sig,lb,ub){
+  ulb <- pnorm(lb,mu,sig)
+  uub <- pnorm(ub,mu,sig)
+  u <- runif(n,ulb,uub)
+  x <- qnorm(u)*sig+mu
+  return(x)
+}
+n <- 10000000
+
+post_param <- read_csv('./VWM/output/results/fit_prev/param_im.csv')
+mu_post <- signif(post_param$mean,2)
+mu_post
+
+sd_post <- signif(post_param$sd,1)
+sd_post
