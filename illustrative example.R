3dab9915aa23309f84136e9cbc49260a75b6c155
sombrelux <2423354032@qq.com>
2022-01-09 19:20:29 +0000

    illust

--- illustrative example.R
+++ illustrative example.R
@@ -3,9 +3,9 @@ library(tidyverse)
 n <- 100
 N <- 100000
 
-prob_real <- 1/(1+exp(-1))
-resp_real <- rbinom(n*N,1,prob_real)
-resp_real <- matrix(resp_real,nrow=n,ncol=N)
+prob_high <- 1/(1+exp(-1))
+resp_high <- c(rbinom(n*N/2,1,prob_high),rep(1,n*N/2))
+resp_high <- matrix(resp_high,nrow=n,ncol=N,byrow = T)
 resp_vague <- matrix(nrow=n,ncol=N)
 
 for(i in 1:n){
@@ -14,10 +14,10 @@ for(i in 1:n){
   resp_vague[i,] <- sapply(prob_vague, function(u) rbinom(1,1,u))
 }
 
-prop_real <- colMeans(resp_real)
+prop_high <- colMeans(resp_high)
 prop_vague <- colMeans(resp_vague)
 
-illust_df <- data.frame(prior='informative',prop=prop_real)%>%
+illust_df <- data.frame(prior='informative',prop=prop_high)%>%
   rbind(.,data.frame(prior='vague',prop=prop_vague))
 ggplot(illust_df,aes(x=prop,group=prior))+
   stat_density(aes(fill=prior),alpha=0.4,bw=0.01)+
